aaadeda9dd75c35d0d3e633ff7ac9f34523dcdc9
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-04 19:17:50 -0300

    refactor: change imports and return type of FakeGeocodeConverter to fit IGeocodeConverter

--- src/infra/geocode/fakes/FakeGeocodeConverter.ts
+++ src/infra/geocode/fakes/FakeGeocodeConverter.ts
@@ -1,10 +1,10 @@
-import { IAddress, ILatLng } from '../../../domain/models/IShipping';
-import { IGeocode } from './../../../data/protocols/geocode/IGeocode';
+import { IAddress } from '../../../domain/models/IShipping';
+import { IGeocode, IAddressWithLatLng } from './../../../data/protocols/geocode/IGeocode';
 
 export class FakeGeocodeConverter implements IGeocode {
-    public async addressToLatLng(address: IAddress): Promise<ILatLng> {
+    public async addressToLatLng(address: IAddress): Promise<IAddressWithLatLng> {
         const addressWithLatLng = Object.assign(address, { lat: 10, lng: 10 });
-        
+
         return new Promise(resolve => resolve(addressWithLatLng));
     }
 }
