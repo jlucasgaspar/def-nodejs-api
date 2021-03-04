6791c8d1bc943c011f68674f9ef74a5d6bdd6376
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-04 18:40:41 -0300

    ensure CreateShipping Controller return 200 if valid params are provided

--- src/presentation/controllers/createShippingController/CreateShippingController.ts
+++ src/presentation/controllers/createShippingController/CreateShippingController.ts
@@ -11,33 +11,34 @@ export class CreateShippingController implements IController {
     }
 
     public async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-        const errorEmptyField = Validator.ensureFieldsAreNotEmpty({
-            httpRequest: httpRequest,
-            requiredFields: createShippingRequiredFields
-        });
-        
-        if (errorEmptyField) {
-            //throw new Error('oi');
-            return HttpResponse.badRequest(errorEmptyField);
+        try {
+            const errorEmptyField = Validator.ensureFieldsAreNotEmpty({
+                httpRequest: httpRequest,
+                requiredFields: createShippingRequiredFields
+            });
+            
+            if (errorEmptyField) {
+                return HttpResponse.badRequest(errorEmptyField);
+            }
+
+            const invalidDateError = Validator.ensureDateTypeIsValid(httpRequest.body.date);
+
+            if (invalidDateError) {
+                return HttpResponse.badRequest(invalidDateError);
+            }
+
+            const { customerName, date, arrivalAddress, departureAddress } = httpRequest.body;
+
+            const shipping = await this.createShippingUseCase.execute({
+                customerName: customerName,
+                date: date,
+                arrivalAddress: arrivalAddress,
+                departureAddress: departureAddress
+            })
+
+            return HttpResponse.ok(shipping);
+        } catch (error) {
+            return HttpResponse.serverError()
         }
-
-        const invalidDateError = Validator.ensureDateTypeIsValid(httpRequest.body.date);
-
-        if (invalidDateError) {
-            return HttpResponse.badRequest(invalidDateError);
-        }
-
-        const { customerName, date, arrivalAddress, departureAddress } = httpRequest.body;
-
-        await this.createShippingUseCase.execute(httpRequest.body);
-        
-        /* const shipping = await this.createShippingUseCase.execute({
-            customerName: customerName,
-            date: date,
-            arrivalAddress: arrivalAddress,
-            departureAddress: departureAddress
-        }) */
-
-        return new Promise(res => res(null));
     }
 }
