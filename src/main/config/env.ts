9421d0d8ef1cecc51da05c3401785b96f287b429
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-05 16:07:11 -0300

    fix: remove env file

--- src/main/config/env.ts
+++
@@ -1,5 +0,0 @@
-export const env = {
-    mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/node-test-jose-lucas',
-    port: process.env.PORT ||3030,
-    geocoderApiKey: 'hcQODu1zfPu8yIXBbD5fD2UG9Gi2F4Bu'
-}
