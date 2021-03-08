9277e063d4c4deb54bcf637028ac2ef3ebbf7187
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-08 11:41:51 -0300

    feat: remove unnecessary code

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export const env = {
     mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/node-test-jose-lucas',
     port: process.env.PORT || 3030,
-    geocoderApiKey: 'lksST0ilpY080DmZodzwCJfAvsuuR4ne'
+    geocoderApiKey: '' // your_api_key_here
 }
